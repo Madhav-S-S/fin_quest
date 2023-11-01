644b7084892fc5f6a8a5daa3db776532f5bb4a51
Madhav S S <madhavgs2002@gmail.com>
2023-11-01 18:31:32 +0530

    Now, if there is not enough m coins belonging to  the user, it will show a message saying that the     user does not have enough coins to buy the item.

--- lib/pool_page.dart
+++ lib/pool_page.dart
@@ -47,7 +47,7 @@ class _PoolSelectionPageState extends State<PoolSelectionPage> {
                       minimumSize: Size(250, 170),
                     ),
                     onPressed: () {
-                      checkAndEnterRoom(widget.customerId, pool);
+                      checkAndEnterRoom(context,widget.customerId, pool);
                     },
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -76,32 +76,69 @@ class _PoolSelectionPageState extends State<PoolSelectionPage> {
       ),
     );
   }
- Future<void> checkAndEnterRoom(String customerId, String pool) async {
+Future<void> checkAndEnterRoom(BuildContext context, String customerId, String pool) async {
   final firestore = FirebaseFirestore.instance;
   final roomsCollection = firestore.collection('${pool}_rooms');
   final usersCollection = firestore.collection('users');
   final userDoc = await usersCollection.doc(customerId).get();
-      final handle = userDoc.get('handle');
+  final handle = userDoc.get('handle');
+  final userCoins = userDoc.get('m_coins'); // Get the user's m_coins value
 
-  // Query rooms with available space
-  final vacantRooms = await roomsCollection.where('currentOccupancy', isLessThan: 5).get();
+  // Check if the user has enough coins to join the pool
+  if (userCoins >= int.parse(pool)) {
+    // Query rooms with available space
+    final vacantRooms = await roomsCollection.where('currentOccupancy', isLessThan: 5).get();
 
-  if (vacantRooms.docs.isNotEmpty) {
-    // Find the first available room
-    final roomDoc = vacantRooms.docs[0];
-    final roomId = roomDoc.id;
+    if (vacantRooms.docs.isNotEmpty) {
+      // Find the first available room
+      final roomDoc = vacantRooms.docs[0];
+      final roomId = roomDoc.id;
 
-    // Check if a document with customerId exists inside the 'player_data' subcollection
-    final playerDataDoc = await roomsCollection.doc(roomId).collection('player_data').doc(customerId).get();
-    if (!playerDataDoc.exists) {
-      // Update the room's occupancy and add the customer to 'player_data'
-      await roomsCollection.doc(roomId).update({
-        'currentOccupancy': FieldValue.increment(1),
-        'total_pool':FieldValue.increment(int.parse(pool)),
+      // Check if a document with customerId exists inside the 'player_data' subcollection
+      final playerDataDoc = await roomsCollection.doc(roomId).collection('player_data').doc(customerId).get();
+      if (!playerDataDoc.exists) {
+        // Update the room's occupancy and add the customer to 'player_data'
+        await roomsCollection.doc(roomId).update({
+          'currentOccupancy': FieldValue.increment(1),
+          'total_pool': FieldValue.increment(int.parse(pool)),
+        });
+
+        // Add the customer as a document with customerId as the name inside 'player_data'
+        await roomsCollection.doc(roomId).collection('player_data').doc(customerId).set({
+          'score': 0,
+          'handle': handle,
+          'game_over': false,
+        });
+
+        // Update the user's currentPool and currentRoom in the 'users' collection
+        await usersCollection.doc(customerId).update({
+          'currentPool': pool,
+          'currentRoom': roomId,
+        });
+        // Subtract ${pool} m_coins from the user
+        await usersCollection.doc(customerId).update({
+          'm_coins': FieldValue.increment(-int.parse(pool)),
+        });
+      }
+
+      // Navigate to the room page
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => RoomPage(customerId: customerId), // Pass the room ID to the RoomPage
+        ),
+      );
+    } else {
+      // Create a new room if no vacant rooms are available
+      final newRoom = await roomsCollection.add({
+        'currentOccupancy': 1, // Initialize with the customer
+        'pool': pool,
+        'total_pool': int.parse(pool),
+        // You can add other room properties as needed
       });
 
-      // Add the customer as a document with customerId as the name inside 'player_data'
-      await roomsCollection.doc(roomId).collection('player_data').doc(customerId).set({
+      // Add the customer as a document with customerId as the name inside 'player_data' of the new room
+      await newRoom.collection('player_data').doc(customerId).set({
         'score': 0,
         'handle': handle,
         'game_over': false,
@@ -110,52 +147,43 @@ class _PoolSelectionPageState extends State<PoolSelectionPage> {
       // Update the user's currentPool and currentRoom in the 'users' collection
       await usersCollection.doc(customerId).update({
         'currentPool': pool,
-        'currentRoom': roomId,
+        'currentRoom': newRoom.id,
       });
-      //substract ${pool} m_coins of the user 
+
+      // Subtract ${pool} m_coins from the user
       await usersCollection.doc(customerId).update({
         'm_coins': FieldValue.increment(-int.parse(pool)),
       });
+
+      // Navigate to the room page
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => RoomPage(customerId: customerId), // Pass the room ID to the RoomPage
+        ),
+      );
     }
-
-    // Navigate to the room page
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => RoomPage(customerId: customerId), // Pass the room ID to the RoomPage
-      ),
-    );
   } else {
-    // Create a new room if no vacant rooms are available
-    final newRoom = await roomsCollection.add({
-      'currentOccupancy': 1, // Initialize with the customer
-      'pool': pool,
-      'total_pool': int.parse(pool),
-      // You can add other room properties as needed
-    });
-
-    // Add the customer as a document with customerId as the name inside 'player_data' of the new room
-    await newRoom.collection('player_data').doc(customerId).set({
-      'score': 0,
-      'handle': handle,
-      'game_over': false,
-      
-    });
-
-    // Update the user's currentPool and currentRoom in the 'users' collection
-    await usersCollection.doc(customerId).update({
-      'currentPool': pool,
-      'currentRoom': newRoom.id,
-    });
-
-    // Navigate to the room page
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => RoomPage(customerId: customerId), // Pass the room ID to the RoomPage
-      ),
+    // Notify the user that they don't have enough coins to join the pool
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Insufficient Coins'),
+          content: Text('You do not have enough coins to join this pool.'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
+
 
 }
