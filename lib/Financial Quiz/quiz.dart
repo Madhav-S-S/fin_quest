bb8648c36d49c5f7a065a2f8b5e9004ee81ddaa4
Madhav S S <madhavgs2002@gmail.com>
2023-11-02 16:51:26 +0530

    UI structure changes to quiz

--- lib/Financial Quiz/quiz.dart
+++ lib/Financial Quiz/quiz.dart
@@ -1,12 +1,20 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import
+ 
+'package:flutter/material.dart';
+import
+ 
+'package:firebase_core/firebase_core.dart';
+import
+ 
+'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MaterialApp(
     home: QuizPage(),
+    theme: ThemeData.light(),
+    darkTheme: ThemeData.dark(),
   ));
 }
 
@@ -74,9 +82,11 @@ class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Quiz App'),
-      ),
+      // Remove the appbar
+      // appBar: AppBar(
+      //   title: Text('Quiz App'),
+      // ),
+
       body: questions.isEmpty
           ? Center(
               child: CircularProgressIndicator(),
@@ -84,26 +94,35 @@ class _QuizPageState extends State<QuizPage> {
           : SingleChildScrollView(
               child: Column(
                 children: [
+                  SizedBox(height: 100),
                   Padding(
                     padding: EdgeInsets.all(16),
-                    child: Text(
-                      'Question ${currentQuestionIndex + 1}: ${questions != null && questions[currentQuestionIndex] != null ? questions[currentQuestionIndex]['question'] : ''}',
-                      style: TextStyle(fontSize: 20),
+                    child: Container(
+                      child: Text(
+                        'Question ${currentQuestionIndex + 1}: ${questions != null && questions[currentQuestionIndex] != null ? questions[currentQuestionIndex]['question'] : ''}',
+                        style: TextStyle(fontSize: 20),
+                      ),
                     ),
                   ),
                   ListView.builder(
                     itemCount: 4,
                     shrinkWrap: true,
-                    itemBuilder: (context, index) => ListTile(
-                      title: GestureDetector(
-                        onTap: () => handleOptionSelection(index),
-                        child: Text(
-                          questions != null &&
-                                  questions[currentQuestionIndex] != null &&
-                                  questions[currentQuestionIndex]['options'] != null
-                              ? questions[currentQuestionIndex]['options'][index]
-                              : '',
-                          style: TextStyle(fontSize: 18),
+                    itemBuilder: (context, index) => Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black12),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: ListTile(
+                        title: GestureDetector(
+                          onTap: () => handleOptionSelection(index),
+                          child: Text(
+                            questions != null &&
+                                questions[currentQuestionIndex] != null &&
+                                questions[currentQuestionIndex]['options'] != null
+                                ? questions[currentQuestionIndex]['options'][index]
+                                : '',
+                            style: TextStyle(fontSize: 18),
+                          ),
                         ),
                       ),
                     ),
